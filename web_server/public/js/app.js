5bdad792583853e1c3f4bd37fcf92b74cd398898
Niraj Kumar <nir8083@gmail.com>
2021-07-19 13:53:15 +0530

    weather continued

--- web_server/public/js/app.js
+++ web_server/public/js/app.js
@@ -1,1 +1,55 @@
 console.log('client side js is loading')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then( (data) => {
+//         console.log(data)
+//     })
+// })
+
+// fetch('http://localhost:3000/weather?address=!').then((response) =>{
+//     response.json().then((data) => {
+//         if(data.error){
+//             console.log(data.error)
+//         }
+//         else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+
+//     })
+
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From javascript'
+
+weatherForm.addEventListener('submit',(e) =>{
+
+    e.preventDefault()
+
+    const location = search.value
+
+    // console.log(location)
+
+    messageOne.textContent='Loading...'
+    messageTwo.textContent=''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
+        response.json().then((data) => {
+            if(data.error){
+                // 
+                messageOne.textContent = data.error
+            }
+            else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                // console.log(data.location)
+                // console.log(data.forecast)
+            }
+        })
+    })
+})
--- web_server/src/app.js
+++ web_server/src/app.js
@@ -40,7 +40,7 @@
 // })
    
 
-// app.get('/weatherr', (req, res) => {
+// app.get('/weather', (req, res) => {
 //     res.send('<h1>Your weather</h1>')
 //    })
 
@@ -48,9 +48,11 @@
 //     console.log('server is up on port 3000')
 // })
 
-const path = require('path')
+const path = require('path') //path is core node modeule so that we can export directly html css file from a seperate folder
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -68,35 +70,80 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index',{
+        title: 'weather',
+        name:'Niraj'
+    })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Niraj'
     })
 })
 
-app.get('/help', (req, res) => {
-    res.render('help', {
+app.get('/help', (req, res) => { //'/help' this is for help is the page we are working with
+    res.render('help', { //'help' this is for the help.hbs
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Niraj'
     })
 })
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'error! You must provide a valid location address'
+        })
+    }
+
+    //
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {} ) => {
+        if(error){
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+            
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    //
+
+    // console.log(req.query.address)
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){ //yadi search query galat rahega to error print
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        products: []
     })
 })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Niraj',
         errorMessage: 'Help article not found.'
     })
 })
@@ -104,11 +151,11 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Niraj',
         errorMessage: 'Page not found.'
     })
 })
 
-app.listen(3000, () => {
+app.listen(3000, () => { //app.listen... to startup the server
     console.log('Server is up on port 3000.')
 })
